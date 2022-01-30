56b295747ab25eeeb097a993531d09f9a6f4ef71
ajvadntr <91082660+ajvadntr@users.noreply.github.com>
2022-01-30 21:24:32 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,37 +11,37 @@ from .Channel import (
 )
 
 HELP = """
-<b><u>Basic Commads</u></b>
+<b><u>Bᴀsɪᴄ Cᴏᴍᴍᴀᴅs</u></b>
 
-➥ /start : check if am alive or dead
-➥ /about : About Me
-➥ /stats user Status
+➥ /start : Cʜᴇᴄᴋ Iꜰ Aᴍ Aʟɪᴠᴇ
 
-<b><u>Bot Owner Only</u></b>
+➥ /about : Aʙᴏᴜᴛ Mᴇ
 
-➥ /broadcast Replay Any Message or Media
-➥ /ban_user  click ban_user more info
-➥ /unban_user click unban_user more info
-➥ /banned_users Banned User Details
-➥ /total How Many Files Added In Database
-➥ /logger  Get Logs
-➥ /delete Delete File From Database
+➥ /status : Usᴇʀ Sᴛᴀᴛᴜs
 
-<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT></b>
+Pᴏᴡᴇʀᴅ Bʏ : @AIOM_BOTS
 """
 
 ABOUT = """
-➥ Name : Auto Filter With IMDB
-    
-➥ Credits : Subin
-
-➥ Editing : [Muhammed RK](t.me/Mrk_YT)
-
-➥ Language : Python3
-
-➥ Library : Pyrogram Asyncio 1.13.0
-
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+╭────[ Vɪɴᴄᴇɴᴢᴏ ]───⍟
+│
+├ Bᴏᴛ Nᴀᴍᴇ : [Vɪɴᴄᴇɴᴢᴏ](https://t.me/VINCENZO_CASSANOS_BOT)
+│
+├ Cʜᴀɴɴᴇʟ : [ƛƖƠM ƁƠƬƧ](https://t.me/AIOM_BOTS)
+│
+├ Vᴇʀsɪᴏɴ : 1.0.2.2  Bᴇᴛᴀ
+│
+├ Sᴏᴜʀᴄᴇ : [Cʟɪᴄᴋ Hᴇʀᴇ](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+│
+├ Sᴇʀᴠᴇʀ : [Hᴇʀᴏᴋᴜ](https://www.heroku.com)
+│
+├ Dᴇᴠᴇʟᴏᴘᴇʀ : [Cʟɪᴄᴋ Hᴇʀᴇ](https://t.me/PR0FESS0R_99)
+│
+├ Eᴅɪᴛɪɴɢ : [Cʟɪᴄᴋ Hᴇʀᴇ](https://t.me/ajvadntr2)
+│
+├ Pᴏᴡᴇʀᴇᴅ Bʏ : [ƛƖƠM ƁƠƬƧ](https://t.me/AIOM_BOTS
+│
+├ Cʀᴇᴅɪᴛꜱ : Sᴜʙɪɴ Pʀᴏꜰᴇꜱꜱᴏʀ-99
+│
+╰──────[ Tʜᴀɴᴋ Yᴏᴜ ]───⍟
 """
