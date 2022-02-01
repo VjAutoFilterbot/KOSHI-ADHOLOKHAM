2caed21a30091f4f7517f73a3dfdcbee8ff49f6d
ajvadntr <91082660+ajvadntr@users.noreply.github.com>
2022-02-01 17:14:09 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -63,7 +63,7 @@ async def filter(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"pr0fess0r_99#{file_id}")]
                     )
         else:
-            await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
+            await client.send_sticker(chat_id=message.from_user.id, sticker='CAACAgIAAxkBAAEDyCRh-OL5KCTPL820bbX7ayBF17MZfwAC8gAD9wLID7_aPYqyARjKIwQ')
             return
 
         if not btn:
